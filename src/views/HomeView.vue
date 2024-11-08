ca314bdb9a38f7bf988748b8dd02686c42e7205a
Dev-SSS1267 <sams3s1267@gmail.com>
2024-11-08 09:37:29 +0900

    Update layout and add logos in HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,9 @@ import DdayCounter from '@/components/DdayCounter.vue'
 </script>
 
 <template>
-  <div class="absolute inset-0 flex flex-col items-center justify-center z-20 text-center text-white space-y-6">
+  <div class="absolute inset-0 flex flex-col items-center justify-center z-20 text-center text-white space-y-4">
     <!-- 상단 문구 -->
-    <div class="text-xl md:text-4xl font-semibold mb-[5rem]">
+    <div class="text-2xl md:text-4xl font-semibold mb-[5rem]">
       believe in yourself, and your dreams will come true.
     </div>
 
@@ -13,10 +13,14 @@ import DdayCounter from '@/components/DdayCounter.vue'
     <DdayCounter />
   </div>
   <!-- 하단 로고 배치 -->
-  <div class="z-20 absolute bottom-5 w-full flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-20 pb-4">
-    <img src="https://cdn.lunaiz.com/kghs/kghs.svg" alt="경기고등학교" class="w-[7.5rem] md:w-[13rem]" />
-    <img src="https://cdn.lunaiz.com/kghs/khbs.svg" alt="경기고등학교 방송부" class="w-[5rem] md:w-[7.5rem]" />
-    <img src="https://cdn.lunaiz.com/lunaiz_logo/logo-d.svg" alt="lunaiz" class="w-[5rem] md:w-[7.5rem]" />
+  <div class="absolute bottom-0 z-20 w-full py-4 flex justify-center">
+    <div
+      class="grid grid-cols-2 md:grid-cols-4 gap-4 items-center">
+      <object data="https://cdn.lunaiz.com/kghs/kghs.svg" alt="경기고등학교" class="w-[7.5rem] md:w-[13rem] mx-auto" />
+      <object data="https://cdn.lunaiz.com/kghs/khbs.svg" alt="경기고등학교 방송부" class="w-[5rem] md:w-[7.5rem] mx-auto" />
+      <object data="https://cdn.lunaiz.com/lunaiz_logo/logo-d.svg" alt="lunaiz" class="w-[5rem] md:w-[7.5rem] mx-auto" />
+      <object data="https://cdn.lunaiz.com/lunaiz_assets/eduflow-d.svg" alt="lunaiz" class="w-[6rem] md:w-[9rem] mx-auto" />
+    </div>
   </div>
 </template>
 
