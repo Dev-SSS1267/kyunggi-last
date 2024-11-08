b6c538800f7f42da3729d4ab8df69e3c72e823a8
Dev-SSS1267 <sams3s1267@gmail.com>
2024-11-08 09:40:47 +0900

    Update video background and text styles in App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { RouterView } from 'vue-router'
 <template>
   <div class="absolute z-10 w-auto min-w-full min-h-full bg-black opacity-70">
   </div>
-  <video class="object-cover absolute z-1 w-auto min-w-full min-h-full" autoplay muted loop id="intro">
+  <video class="object-cover absolute z-1 w-auto min-w-full min-h-full" autoplay muted loop playsinline id="intro">
     <source src="https://cdn.lunaiz.com/kghs/kyunggi_introduce.mp4" type="video/mp4">
   </video>
   <RouterView />
